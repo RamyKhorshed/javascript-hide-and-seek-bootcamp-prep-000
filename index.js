4a5305cbab30bc579510a8150b17de65ee92056b
Ramy Khorshed <RamyKhorshed@gmail.com>
2017-11-06 14:29:08 -0500

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function increaseRankBy(n) {
 
   for (let i = 0, l = rankedLists.length; i < l; i++) {
     let children = rankedLists[i].children
-
     for (let j = 0, k = children.length; j < k; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n
     }
